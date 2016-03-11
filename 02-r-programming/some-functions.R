02bc904459ce68413da1f3fb315a996d99e6cb58
Arthur Gouveia <gouveia.arthur@gmail.com>
2016-03-10 21:58:44 -0300

    After Swirl practice and first assignment

--- 02-r-programming/some-functions.R
+++ 02-r-programming/some-functions.R
@@ -1,19 +1,19 @@
 #Soma dois valores
 add2 <- function(x, y) {
-    x + y
+      x + y
 }
 
 #Retorna os numeros de vect que sao maiores que n 
 above <- function(vect, n = 10) {
-  vect[vect > n]
+      vect[vect > n]
 }
 
 #Calcula a media dos dados de cada coluna de df
 columnmeans <- function(df, removeNA = TRUE) {
-    nc <- ncol(df)          #numero de colunas
-    means <- numeric(nc)    #vetor que irá conter as medias
-    for(i in 1:nc) { #loop por todas as colunas armazenando as medias
-        means[i] <- mean(df[,i], na.rm = removeNA)
-    }
-    means
+      nc <- ncol(df)          #numero de colunas
+      means <- numeric(nc)    #vetor que irá conter as medias
+      for(i in 1:nc) { #loop por todas as colunas armazenando as medias
+            means[i] <- mean(df[,i], na.rm = removeNA)
+      }
+      means
 }
